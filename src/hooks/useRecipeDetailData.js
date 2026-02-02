f024aefcc8b38a20b088e6f3b1a1cc62d4e5a28a
Dante0214 <96513476+Dante0214@users.noreply.github.com>
2026-02-02 16:14:42 +0900

    fix: url 오류 수정

--- src/hooks/useRecipeDetailData.js
+++ src/hooks/useRecipeDetailData.js
@@ -1,8 +1,26 @@
+// import { useQuery } from "@tanstack/react-query";
+// import api from "../utils/api";
+
+// const fetchRecipeDetailData = (recipeName) => {
+//   return api.get(`1/50/RCP_NM=${recipeName}`);
+// };
+
+// export const useRecipeDetailDataQuery = (recipeName) => {
+//   return useQuery({
+//     queryKey: ["recipe-detail-data", recipeName],
+//     queryFn: () => fetchRecipeDetailData(recipeName),
+//     select: (result) => result.data.COOKRCP01.row[0],
+//   });
+// };
 import { useQuery } from "@tanstack/react-query";
 import api from "../utils/api";
 
+const normalizeRecipeName = (name) =>
+  decodeURIComponent(name).replace(/\s+/g, "");
+
 const fetchRecipeDetailData = (recipeName) => {
-  return api.get(`1/50/RCP_NM=${recipeName}`);
+  const normalizedName = normalizeRecipeName(recipeName);
+  return api.get(`1/50/RCP_NM=${normalizedName}`);
 };
 
 export const useRecipeDetailDataQuery = (recipeName) => {
@@ -10,5 +28,6 @@ export const useRecipeDetailDataQuery = (recipeName) => {
     queryKey: ["recipe-detail-data", recipeName],
     queryFn: () => fetchRecipeDetailData(recipeName),
     select: (result) => result.data.COOKRCP01.row[0],
+    enabled: !!recipeName,
   });
 };
