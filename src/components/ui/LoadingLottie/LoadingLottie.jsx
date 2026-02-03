610a8d064bb829df6e039a903d51a9d363a701c0
Dante0214 <rn_chic@naver.com>
2026-02-03 17:04:58 +0900

    featL 로딩로티 변경

--- src/components/ui/LoadingLottie/LoadingLottie.jsx
+++ src/components/ui/LoadingLottie/LoadingLottie.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { PuffLoader } from "react-spinners";
+import Lottie from "lottie-react";
+import loadingAnimation from "./assets/LoadingLottie_x2.json";
 
 const LoadingLottie = () => {
   return (
-    <div className="flex justify-center items-center w-full h-full">
-      <PuffLoader color="#ed0c0c" size={100} />
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Lottie
+        animationData={loadingAnimation}
+        loop
+        autoplay
+        style={{ width: 140, height: 140 }} // 크기를 살짝 키워도 좋겠네요!
+      />
+      <p className="mt-2 text-lg font-semibold text-orange-500">
+        맛있는 데이터를 불러오는 중... 👨‍🍳
+      </p>
     </div>
   );
 };
